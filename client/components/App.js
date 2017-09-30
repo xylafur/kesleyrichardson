c54f674d79e7c6cd6473a8c538dc52b947466d42
ceciliamvrie <cecilia.mvrie@gmail.com>
2017-09-30 18:19:39 -0500

    added footer

--- client/components/App.js
+++ client/components/App.js
@@ -3,6 +3,7 @@ import Home from './Home'
 import Shows from './Shows.jsx'
 import Music from './Music'
 import Writings from './Writings'
+import Footer from './Footer'
 import events from '../data/events'
 
 class App extends React.Component {
@@ -15,6 +16,7 @@ class App extends React.Component {
         <ul style={{'list-style': 'none'}}>
           <li><Writings /></li>
         </ul>
+        <Footer />
       </div>
     )
   }
---
+++ client/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+      <div className="footer">
+        <p>Copyright 2017 <span className="my-link">Cecilia Cisneros</span></p>
+      </div>
+    )
+}
+
+export default Footer

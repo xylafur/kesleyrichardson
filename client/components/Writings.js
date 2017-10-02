ee909ea9f999b4b9de2a11096ee3b65344f5e3e8
ceciliamvrie <cecilia.mvrie@gmail.com>
2017-10-02 17:57:46 -0500

    finished somemore styling

--- client/components/Writings.js
+++ client/components/Writings.js
@@ -8,12 +8,14 @@ class Writings extends React.Component {
 
   render () {
     return (
-      <div className="writings" style={{"backgroundImage": "url(" + "'" + layer4 + "')", "backgroundSize":"cover", "backgroundColor": "rgba(0,0,0)", "opacity": "0.6", "backgroundAttachment": "fixed", "backgroundRepeat": "no-repeat", "backgroundSize": "cover"}}>
+      <div style={{"backgroundImage": "url(" + "'" + layer4 + "')", "backgroundSize":"cover", "opacity": "0.8", "backgroundAttachment": "fixed", "backgroundRepeat": "no-repeat"}}>
+      <div className="writings overlay5">
         <h1 className="writings-kicker">Travels, Prose, and Poetry (AMANDAPASCALI.BLOGSPOT.COM)</h1>
         <h3 className="writings-quote">"I WAS BORN ALONG A DASHED, YELLOW LINE THAT RUNS STRAIGHT THROUGH THE HIGHWAY IN A LAND THAT ISN'T MINE."</h3>
         <p className="final-quote">"To me, instant coffee, the kind that comes in those little packets, the powder that you pour under
 boiling hot water, tastes like Italy. And it tastes like my grandmother's apartment in Eastern Europe, the one with drawers full of old Communist love letters and where me and my father would hang our cleanest dirty clothes above the shower. Those instant coffee packets taste like the late nights in Milan when we didn't have enough money for an espresso. It tastes like wondering where you will sleep the next night. And where you'll be tomorrow."</p>
       </div>
+    </div>
     )
   }
 }

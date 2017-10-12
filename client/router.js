b4c425b61e557d1f5c45886739906a9ec76f1b98
ceciliamvrie <cecilia.mvrie@gmail.com>
2017-10-12 16:42:43 -0500

    fixed router mistake

--- client/router.js
+++ client/router.js
@@ -6,19 +6,20 @@ import Media from './components/Media'
 import Contact from './components/Contact'
 // import NotFound from './components/NotFound';
 
+const PUBLIC_URL = 'https://github.com/ceciliamvrie/amandapascali.github.io'
 class Routes extends Component {
   render() {
     return (
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router basename={PUBLIC_URL}>
         <div>
           <Route exact path="/" component={App} />
           <Route path="/contact" component={Contact} />
           <Route path="/media" component={Media} />
         </div>
-        {/* <Route path="*" component={NotFound} /> */}
+        {/* <Route path="*" component= /> */}
       </Router>
     )
   }
 }
 
-export default Routes 
+export default Routes

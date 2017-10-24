14d27ed3cd9ae1834ebe3411f478c48b8f7c73a1
ceciliamvrie <cecilia.mvrie@gmail.com>
2017-10-24 14:24:12 -0500

    added to video

--- client/components/Music.js
+++ client/components/Music.js
@@ -18,7 +18,7 @@ class Music extends React.Component {
             <Youtube
               video="VevWC4w6Tb0" autoplay="0" rel="0" modest="1"
             />
-            <span className="album-name"><h2 className="author">Amanda Pascali</h2><h2 className="album-name">SELF TITLED EP</h2></span>
+            <span className="album-name"><h2 className="author">Amanda Pascali</h2><h2 className="album-name">Live Show on KFPT 90.1 FM Radio</h2></span>
             <span className="whoBy">by Amanda Pascali</span>
           </div>
           <SongsList />
--- client/components/Song.js
+++ client/components/Song.js
@@ -13,7 +13,7 @@ class Song extends React.Component {
     return (
       <div className="songsList">
         <h3 className="songName">{this.props.name}</h3>
-        <audio controls preload id="audio" style={{backgroundColor: 'transparent', borderColor: 'transparent', border: 'solid 1px', margin: '2% 0%'}}>
+        <audio controls preload id="audiocontrols" style={{backgroundColor: 'transparent', borderColor: 'transparent', border: 'solid 1px'}}>
         <source src={this.props.mp3} type="audio/mpeg" />
         </audio>
       </div>

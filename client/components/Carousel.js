74aaf109bf68fc99285a3f9fd5a680f3a227d878
ceciliamvrie <cecilia.mvrie@gmail.com>
2017-10-24 11:21:30 -0500

    fixed the carousel

--- client/components/Carousel.js
+++ client/components/Carousel.js
@@ -1,31 +1,19 @@
 import React from 'react'
 import Slider from 'react-slick'
 
-var flowers = require("bundle-loader?lazy!../static/pictures/flowers.jpg")
 var family = require("bundle-loader?lazy!../static/pictures/family.jpg")
-var happyconcert = require("bundle-loader?lazy!../static/pictures/happyconcert.jpg")
 
 class SimpleSlider extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      flowers: null,
       family: null,
-      happyconcert: null
     }
   }
   componentDidMount() {
-    flowers((file) => {
-      console.log(file)
-      this.setState({flowers: file})
-    })
     family((file) => {
       console.log(file)
       this.setState({family: file})
-    })
-    happyconcert((file) => {
-      console.log(file)
-      this.setState({happyconcert: file})
     })
   }
   render () {
@@ -40,9 +28,7 @@ class SimpleSlider extends React.Component {
     };
     return (
       <Slider {...settings} style={{"height": "500px"}}>
-        <div className="overlay-media"><div className="slider" style={{"backgroundImage": `url(${this.state.flowers})`, "backgroundSize": "cover", "opacity": "0.6", "width": "100%", "height": "auto"}}><h1 className="media">Media</h1></div></div>
         <div className="overlay-media"><div className="slider" style={{"backgroundImage": `url(${this.state.family})`, "backgroundSize": "cover", "opacity": "0.6", "width": "100%", "height": "100%"}}><h1 className="media">Media</h1></div></div>
-        <div className="overlay-media"><div className="slider" style={{"backgroundImage": `url(${this.state.happyconcert})`, "backgroundSize": "cover","opacity": "0.6", "width": "100%", "height": "100%"}}><h1 className="media">Media</h1></div></div>
       </Slider>
     )
   }
--- client/components/Home.js
+++ client/components/Home.js
@@ -13,7 +13,6 @@ class Home extends React.Component {
         <div className="header">
           <h1 className="name">Amanda Pascali</h1>
           <h3 className="kicker">Singer/Songwriter, Activist, Student of Natural Sciences.</h3>
-          <button className="header-button">Amanda Pascali's Self Titled EP</button>
         </div>
         <FrontQuote />
       </div>
